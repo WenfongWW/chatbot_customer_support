2edae23bb7a051f736e2f4aca171acc6328b76db
WenfongWW <87782692+WenfongWW@users.noreply.github.com>
2024-01-12 10:03:09 -0400

    Updated openai API

--- Customer Support.py
+++ Customer Support.py
@@ -1,12 +1,26 @@
 from flask import Flask, request, render_template, jsonify 
 import openai
-from transformers import AutoModelForCausalLM, AutoTokenizer
+#from transformers import AutoModelForCausalLM, AutoTokenizer
 
 openai.api_key = "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"
 
-completion = openai
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", message=[{"role":"user", "content":"Write an essay about machine learning"}])
+print(completion.choice[0].message.content)
 
 app = Flask(__name__)
+
+from openai import OpenAI
+client = OpenAI("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")
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
 
 
 @app.route("/")
